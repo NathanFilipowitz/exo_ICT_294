17ddc56c9dfa8f67798a32fc6a37f953540b6e7f
NathanFilipowitz <py58wpb@eduvaud.ch>
2025-11-03 16:12:45 +0100

    added conditionnal paragraphs on the html file using basic v-if v-else v-else-if methods

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,29 @@
 <script setup>
-import { ref } from 'vue'
-import socksGreenImage from './assets/images/socks_green.jpeg'
+import { ref } from "vue";
+import socksGreenImage from "./assets/images/socks_green.jpeg";
 
-const product = ref('Socks')
-const image = ref(socksGreenImage)
-
+const product = ref("Socks");
+const image = ref(socksGreenImage);
+const inventory = ref(100);
+const onSale = ref(true);
 </script>
-  
+
 <template>
-  <div class="nav-bar"></div>
-  <div class="product-display">
-    <div class="product-container">
-      <div class="product-image">    
-        <img v-bind:src="image">
-      </div>
-      <div class="product-info">
-        <h1>{{ product }}</h1>
-      </div>
+    <div class="nav-bar"></div>
+    <div class="product-display">
+        <div class="product-container">
+            <div class="product-image">
+                <img v-bind:src="image" />
+            </div>
+            <div class="product-info">
+                <h1>{{ product }}</h1>
+            </div>
+            <p v-if="inventory > 30">In Stock</p>
+            <p v-else-if="inventory <= 30 && inventory > 0">
+                Almost sold out !
+            </p>
+            <p v-else>Out of Stock</p>
+            <p v-if="onSale">On Sale !!</p>
+        </div>
     </div>
-  </div>
 </template>
