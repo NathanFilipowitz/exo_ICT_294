a0d29dca3005e6c02a7da229b6123e6e448de15f
NathanFilipowitz <py58wpb@eduvaud.ch>
2025-11-03 15:47:18 +0100

    added reactive image and link with text of the link also reactive using new v-text attribute

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,25 @@
 <script setup>
-import { ref } from 'vue'
+import { ref } from "vue";
+import socksGreenImage from "./assets/images/socks_green.jpeg";
 
-const product = ref('Socks')
-
+const product = ref("Socks");
+const image = ref(socksGreenImage);
+const url = ref(
+    "https://www.vuemastery.com/courses/intro-to-vue-3-comp-api/attribute-binding-comp-api",
+);
+const urlText = ref("Guide suivi");
 </script>
 
 <template>
-  <div class="product-display">
-    <div class="product-container">
-      <div class="product-image">
-        <!-- image goes here -->
-      </div>
-      <div class="product-info">
-        <h1>{{ product }}</h1>
-      </div>
+    <div class="product-display">
+        <div class="product-container">
+            <div class="product-image">
+                <img v-bind:src="image" />
+            </div>
+            <div class="product-info">
+                <h1>{{ product }}</h1>
+            </div>
+            <a :href="url" v-text="urlText"></a>
+        </div>
     </div>
-  </div>
 </template>
