acd197191731343bdd931aa59558889d539eed7e
NathanFilipowitz <py58wpb@eduvaud.ch>
2025-11-10 09:54:48 +0100

    fixed product variants using image variable as string and not var.

--- src/App.vue
+++ src/App.vue
@@ -10,8 +10,8 @@ const inStock = true
 const details = ref(['50% cotton', '30% wool', '20% polyester'])
 
 const variants = ref([
-  { id: 2234, color: 'green', image: 'socksGreenImage' },
-  { id: 2235, color: 'blue', image: 'socksBlueImage' },
+  { id: 2234, color: 'green', image: socksGreenImage },
+  { id: 2235, color: 'blue', image: socksBlueImage },
 ])
 
 const cart = ref(0)
