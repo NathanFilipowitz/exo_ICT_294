adf8924e10660fb3409b60f38811f89da9ebe6b3
NathanFilipowitz <py58wpb@eduvaud.ch>
2025-11-10 11:16:37 +0100

    finished exercice and coding challenge, all OK

---
+++ src/components/ProductDetails.vue
@@ -0,0 +1,16 @@
+<script setup>
+import {ref} from "vue";
+
+const props = defineProps({
+  details: {
+    type: Boolean,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <ul>
+    <li v-for="detail in details">{{ detail }}</li>
+  </ul>
+</template>
